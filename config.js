b963b44101405eb217e0b5dcd3762231b0e52c40
nguyenthinhuye <94517838+nguyenthinhuye@users.noreply.github.com>
2021-11-17 16:06:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: ` Nguyễn Thị Như Ý ẹeee`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'em có muốn anh làm chồng em 👉👈😊',
+    desc: 'Lấy anh nhe 💍 , sau này anh sẽ nhường mọi thứ cho em từ nấu cơm , rửa chén , giật đồ , lau nhà , mọi thứ anh sẽ nhường and nhịn với embe hết,... ',
+    btnYes: 'Chịu hổng luôn ớ trời, vẫn cứ phải chịu anh ^^ ',
+    btnNo: 'lát em trả lời anh sau nhé, giờ con em đang khóc :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em muốn lấy anh, khai lịe :3',
+    btnReply: 'Gửi tìn iu của em kkkkk <3 ',
+    reply: 'Lấy thì lấy không lấy thì ìiiíiii  lấy :))))) <3',
+    mess: 'ọi anh biết mà 🥰. Yêu em quóoooooo 😘😘',
+    messDesc: '100 days hẹn suối thác nước chảy nhe bà xõaaaaa.',
+    btnAccept: 'Okiiiii lun, thích quá zạaaa <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
